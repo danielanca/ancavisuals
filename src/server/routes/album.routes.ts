b6ea7cac9bd6eff6b1e6e0c7a0ea8ebbd011d4e1
daniel.anca <daniel.anca@eggs.ro>
2026-01-09 18:36:51 +0200

    Download printed photos selection.

--- src/server/routes/album.routes.ts
+++ src/server/routes/album.routes.ts
@@ -5,7 +5,7 @@ import { postPrintSelection } from "../../utils/album.controller";
 import { downloadAll } from "./../../utils/album.controller";
 import { getAlbumStats } from "../services/album.stats";
 import { deletePhoto } from "../../utils/album.controller";
-
+import { downloadPrintDynamic } from "./../../utils/album.controller";
 const router = Router();
 
 router.get("/:slug", getAlbum);
@@ -15,5 +15,8 @@ router.post("/:slug/download-selected", express.urlencoded({ extended: false }),
 router.post("/:slug/print-selection", express.json(), postPrintSelection);
 router.post("/:slug/download-all", downloadAll);
 router.post("/:slug/delete-photo", express.json(), deletePhoto);
+
+router.post("/:slug/download-print-dynamic", downloadPrintDynamic);
+
 
 export default router;
