b0a8f2642a2b1fdf3362ef52414a8b655bb1c437
daniel.anca <daniel.anca@eggs.ro>
2026-01-09 22:02:16 +0200

    Working on invite.

---
+++ src/client/pages/InvitationLanding/InvitationLanding.tsx
@@ -0,0 +1,15 @@
+import WeddingHero from './WeddingHero';
+
+export default function InvitationLanding() {
+  return (
+    <WeddingHero
+      groom="Daniel"
+      bride="Claudia"
+      date="Saturday, October 30, 2030"
+      time="3:30 PM"
+      venue="Hotel Fauget"
+      address="123 Anywhere St, Any City"
+      phone="+123 456 7890"
+    />
+  );
+}
